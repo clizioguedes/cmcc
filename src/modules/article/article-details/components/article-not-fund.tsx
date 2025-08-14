5e2a1a29b9f3000d18dcf98a884ac5f6a10ab0de
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-08-14 15:23:05 -0300

    feat: change font

--- src/modules/article/article-details/components/article-not-fund.tsx
+++ src/modules/article/article-details/components/article-not-fund.tsx
@@ -2,17 +2,18 @@ import { FileXIcon, HomeIcon } from 'lucide-react'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 export function ArticleNotFound() {
   return (
-    <div className="mt-24 flex min-h-screen justify-center">
+    <div className={cn('mt-8 flex min-h-screen justify-center')}>
       <div className="flex flex-col gap-4 text-center">
-        <div className="flex flex-col gap-4">
-          <FileXIcon className="text-muted-foreground/50 mx-auto size-24" />
+        <div className="flex flex-col items-center gap-4">
+          <FileXIcon className="text-muted-foreground/50 size-24" />
 
-          <h1 className="text-2xl font-bold">Notícia não encontrada</h1>
+          <h1 className="text-2xl font-semibold">Notícia não encontrada</h1>
 
-          <p className="text-muted-foreground mx-auto max-w-2xl text-lg">
+          <p className="text-muted-foreground max-w-2xl text-lg">
             Desculpe, não conseguimos encontrar a notícia que você procura. Ele
             pode ter sido movido, excluído ou o URL pode estar incorreto.
           </p>
