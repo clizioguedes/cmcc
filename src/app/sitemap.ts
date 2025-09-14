1e6fd8265f7b9d5d1c6610c6e15866df58bb249f
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-09-14 15:09:34 -0300

    fix: comment get articles in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,10 @@
 import type { MetadataRoute } from 'next'
 
 import { env } from '@/env'
-import { getArticles } from '@/http/requests/get-articles'
+// import { getArticles } from '@/http/requests/get-articles'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { data: articles } = await getArticles()
+  // const { data: articles } = await getArticles()
 
   const baseUrl = env.NEXT_PUBLIC_BASE_URL.replace(/\/$/, '')
 
@@ -17,12 +17,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     priority: 1,
   }))
 
-  const dynamicPages: MetadataRoute.Sitemap = articles.map((article) => ({
-    url: `${baseUrl}/artigos/${article.slug}`,
-    lastModified: new Date(article.updatedAt),
-    changeFrequency: 'yearly',
-    priority: 0.8,
-  }))
+  // const dynamicPages: MetadataRoute.Sitemap = articles.map((article) => ({
+  //   url: `${baseUrl}/artigos/${article.slug}`,
+  //   lastModified: new Date(article.updatedAt),
+  //   changeFrequency: 'yearly',
+  //   priority: 0.8,
+  // }))
 
-  return [...staticPages, ...dynamicPages]
+  // return [...staticPages, ...dynamicPages]
+  return [...staticPages]
 }
