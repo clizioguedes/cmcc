13c9105b49f421026341360fcc2f970a1d1994aa
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-09-15 14:37:30 -0300

    feat: add tmp strapi in config image

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,11 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'i8gkck8ww8ocwccgc8048c4k.82.25.68.46.sslip.io',
+        pathname: '/**',
+      },
     ],
   },
 }
