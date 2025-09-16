b5109af3b6245734f9d133fdd3d90b2d7977b2e9
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-09-16 09:43:32 -0300

    feat: create dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  reactStrictMode: false,
+  compress: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
